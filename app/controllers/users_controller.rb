d8cbcbcb90f7db9de166d55eafbf385b231f39e0
ryoya.ebisawa <ryoya.ebisawa@gmail.com>
2020-02-02 12:44:20 +0000

    kadai fix likes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers , :favoritings, :favoriters]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers , :favoritings, :favoriters ,:likes]
   
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
